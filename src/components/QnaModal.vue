d03341e7897f6f5b9581c9a4123301799051a209
Chang Hyun Yoo <jakey1110@naver.com>
2022-11-23 13:11:12 +0900

    create backoffice

---
+++ src/components/QnaModal.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="wrapper">
+        <div class="modal-back">
+            <div style="width: 100%; height: 1.7em;">
+                <button class="close">X</button>
+            </div>
+            <div class="question">
+                현재 대학교에서 영상
+            </div>
+            <div style="width: 90%; height: 10%;margin: auto;">
+                <button class="btn">답변 달기</button>
+                <button class="btn">답변 수정</button>
+            </div>
+            <div class="answer">
+                <div style="width: 90%; height: 90%; padding: 1em;margin: auto">
+                    안됩니다
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { ref, watch, getCurrentInstance } from 'vue';
+export default {
+    props: {
+        space: {
+            type: Object,
+            required: true,
+        }
+    },
+    setup() {
+    }
+}
+</script>
+
+<style scoped>
+.wrapper {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 100;
+}
+.modal-back {
+    width: 70%;
+    min-width: 700px;
+    height: 60%;
+    min-height: 500px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    font-weight: bold;
+}
+.close {
+    border: none;
+    background-color: white;
+    float: right;
+    cursor: pointer;
+}
+.question {
+    width: 90%;
+    height: 40%;
+    margin: auto;
+    color: #858589;
+}
+.answer {
+    width: 90%;
+    height: 40%;
+    margin: auto;
+    color: #858589;
+    background-color: #F1EEEE;
+    border-radius: 1em;
+}
+.btn {
+    padding: 1% 3%;
+    background-color: #041461;
+    color: white;
+    border: none;
+    border-radius: 2em;
+    cursor: pointer;
+    float: right;
+}
+.btn:hover {
+    background-color: skyblue;
+}
+</style>
---
+++ src/pages/backOffice/dashBoard.vue
@@ -0,0 +1,117 @@
+<template>
+    <div style="display:flex">
+        <MenuBar />
+        <div class="content">
+            <div class="status">
+                <div class="reserved">
+                    <div style="font-weight: bold;">
+                        금일 예약 5 건
+                    </div>
+                </div>
+                <div class="canceled">
+                    <div style="font-weight: bold;">
+                        금일 취소 예약 2 건
+                    </div>
+                </div>
+            </div>
+
+            <div class="calender">
+
+            </div>
+
+            <div class="memo">
+                <div class="title">
+                    관리 메모
+                </div>
+                <div class="memos">
+                    <div v-for="num in 10" :key="num" class="one">
+                        {{num}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import MenuBar from './menuBar.vue';
+export default {
+    components: {
+        MenuBar,
+    }
+}
+</script>
+
+<style scoped>
+.content {
+    width: 85%;
+    height:100vh;
+    overflow:scroll;
+    overflow-x: hidden;
+    overflow-y: auto;
+    text-align: center;
+}
+.status {
+    width: 50%;
+    height: 4em;
+    margin: 1em auto;
+}
+.reserved {
+    width: 40%;
+    height: 100%;
+    background-color:#EDF8E7;
+    float: left;
+    text-align: center;
+    border-radius: 1em;
+    display : flex;
+    justify-content: center;
+    align-items : center;
+}
+.canceled {
+    width: 40%;
+    height: 100%;
+    background-color:#FCDDDD;
+    float: right;
+    text-align: center;
+    border-radius: 1em;
+    display : flex;
+    justify-content: center;
+    align-items : center;
+}
+.calender {
+    width: 80%;
+    height: 27em;
+    background-color:#EDF8E7;
+    margin: 2em auto;
+}
+.memo {
+    width: 80%;
+    height: 10em;
+    margin: 3em auto;
+}
+.title {
+    width: 100%;
+    min-height: 20px;
+    height: 25%;
+    background-color:#F1EEEE;
+    border-top-left-radius: 1em;
+    border-top-right-radius: 1em;
+    color: #6C6969;
+    font-weight: bold;
+    display : flex;
+    justify-content: center;
+    align-items : center;
+}
+.memos {
+    width: 100%;
+    height: 75%;
+    background-color:#FFFCFC;
+    border-bottom-left-radius: 1em;
+    border-bottom-right-radius: 1em;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    overflow: scroll;
+    overflow-x: hidden;
+    overflow-y: auto;
+}
+</style>
