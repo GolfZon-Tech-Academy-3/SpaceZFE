738aefb45c0d9b2c399a1e2b0fe8ece4c39ab826
Chang Hyun Yoo <jakey1110@naver.com>
2022-12-12 14:46:15 +0900

    change axios

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ export default function (app) {
   app.use(
     createProxyMiddleware("/api", {
       // 백엔드 주소
-      target: "https://spacez3.shop",
+      target: "https://spacez3.shop/",
       changeOrigin: true,
     })
   );
