39e173c24786bc94b157b93cf55ba6e4cf395c92
Chang Hyun Yoo <jakey1110@naver.com>
2022-12-12 13:59:59 +0900

    change settings

--- vue.config.js
+++ vue.config.js
@@ -5,11 +5,11 @@ module.exports = defineConfig({
   lintOnSave: false,
   devServer: {
     proxy: 'https://spacez3.shop/',
-    https: {
-      key: fs.readFileSync('../ssl/domain.com.key'),
-      cert: fs.readFileSync('../ssl/domain.com.crt'),
-      ca: fs.readFileSync('../ssl/rootca.crt'),
-    }
+    // https: {
+    //   key: fs.readFileSync('../ssl/domain.com.key'),
+    //   cert: fs.readFileSync('../ssl/domain.com.crt'),
+    //   ca: fs.readFileSync('../ssl/rootca.crt'),
+    // }
   }
 })
 
